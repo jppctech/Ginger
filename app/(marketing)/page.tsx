7b2dcff1d1daba38107151cac5b8e7cf1d4e78ad
jppctech <dilipmorigaon@gmail.com>
2023-12-11 14:17:27 +0530

    ginger

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,53 @@
+import { Medal } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import localFont from "next/font/local";
+import { cn } from "@/lib/utils";
+
+
+const headingFont =localFont({
+    src: "../../public/fonts/font.woff2"
+})
+
+const MarketingPage = () =>{
+    return(
+        <div className="flex items-center justify-center flex-col" >
+            <div className={cn(
+                "flex items-center justify-center flex-col",
+                 headingFont.className
+                )} >
+                <div className="mb-4 flex items-center border shadow-sm p-4
+                bg-amber-100 text-amber-700 rounded-full uppercase">
+                    <Medal className=" h-6 w-6 mr-2" />
+                    No 1 task managment
+                    </div>
+                        <h1 className="text-3xl md:text-6xl text-center
+                        text-neutral-800 mb-6">
+                            Taskify help team move
+                        </h1>
+                        <div className="text-3xl md:-6xl bg-gradient-to-r
+                        from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pd-4
+                        w-fit">
+                            work forward.
+                        </div>
+                    </div>
+                    <div className="text-sm md:text-xl text-neutral-400 mt-4 max-w-xs
+                    md:max-w-2xl text-center mx-auto">
+                    Amber is fossilized tree resin that has
+                    been appreciated for its color and natural
+                    beauty since Neolithic times. Much valued 
+                    from antiquity to the present as a gemstone,
+                    amber is made into a variety of decorative 
+                    objects. Amber is used in jewelry and has been
+                     used as a healing agent in folk medicine.
+                    </div>
+                    <Button className="mt-6  size=lg aschild">
+                        <Link href="/sign-up">
+                            Get Taskify for free
+                        </Link>
+                    </Button>
+        </div>
+    );
+};
+
+export default MarketingPage;
