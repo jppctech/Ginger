4a71c46253a16a91a09f3f78bb7182584ce9ca33
jppctech <dilipmorigaon@gmail.com>
2023-12-31 14:27:20 +0530

    changes

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,28 @@
 const OraganizationIdPage = () =>{
-return(
+  async function create(formData: FormData) {
+    "use server";
+    
+    const title = formData.get("title") as string;
+    await db.board.create({
+        data:{
+            title
+        }
+    });
+  }
+
+
+    return(
     <div>
-        Organization Page
+        <form>
+            <input
+            id="title"
+            name="title"
+            required
+            placeholder="Enter a board title"
+            className="border-black border p-1">
+            
+            </input>
+        </form>
     </div>
 );
 };
